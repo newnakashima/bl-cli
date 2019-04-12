a0b701e194598a373acd5f491f816f44062cc807
newnakashima <newnakashima2014@gmail.com>
2019-04-13 08:29:11 +0900

    修正

--- create_example.py
+++ create_example.py
@@ -8,12 +8,10 @@ HOME = expanduser("~")
 config = configparser.ConfigParser()
 
 parser = OptionParser()
-parser.add_option("-o", "--output", dest="Output path",
+parser.add_option("-o", "--output", dest="output",
         help="OUTPUT is output path of configuration file",
         metavar="OUTPUT")
 (options, args) = parser.parse_args()
-print(options)
-print(args)
 
 try:
     print("Please input your backlog url: ")
@@ -26,13 +24,16 @@ try:
         'access_key': access_key,
     }
 
-    path = HOME + '/.bl'
-    if not os.path.exists(path):
-        os.makedirs(path)
-    if not os.path.isdir(path):
-        sys.exit(f'Path {path} is not a directory. Please make sure the path is a directory.')
-        
-    with open(path + '/credentials', 'w') as configfile:
+    if options.output is None:
+        path = HOME + '/.bl'
+        conffile = path + '/credentials'
+        if not os.path.exists(path):
+            os.makedirs(path)
+        if not os.path.isdir(path):
+            sys.exit(f'Path {path} is not a directory. Please make sure the path is a directory.')
+    else:
+        conffile = options.output
+    with open(conffile, 'w') as configfile:
         config.write(configfile)
 except KeyboardInterrupt:
     print("Configuration dialogue has stopped.")

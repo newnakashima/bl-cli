73ec4c332853217dd030e9cceb0af70d7d77891c
newnakashima <newnakashima2014@gmail.com>
2019-04-12 13:41:50 +0900

    オプションでコンフィグファイルの出力先を指定できるようにしたい

--- create_example.py
+++ create_example.py
@@ -1,15 +1,38 @@
 import configparser
+import os
+from os.path import expanduser
+import sys
+from optparse import OptionParser
 
+HOME = expanduser("~")
 config = configparser.ConfigParser()
 
-print("Please input your backlog url: ")
-base_url = input()
-print("Please input your backlog access key: ")
-access_key = input()
+parser = OptionParser()
+parser.add_option("-o", "--output", dest="Output path",
+        help="OUTPUT is output path of configuration file",
+        metavar="OUTPUT")
+(options, args) = parser.parse_args()
+print(options)
+print(args)
 
-config['nksm'] = {
-    'base_url':base_url,
-    'access_key': access_key,
-}
-with open('example.ini', 'w') as configfile:
-    config.write(configfile)
+try:
+    print("Please input your backlog url: ")
+    base_url = input()
+    print("Please input your backlog access key: ")
+    access_key = input()
+
+    config['nksm'] = {
+        'base_url':base_url,
+        'access_key': access_key,
+    }
+
+    path = HOME + '/.bl'
+    if not os.path.exists(path):
+        os.makedirs(path)
+    if not os.path.isdir(path):
+        sys.exit(f'Path {path} is not a directory. Please make sure the path is a directory.')
+        
+    with open(path + '/credentials', 'w') as configfile:
+        config.write(configfile)
+except KeyboardInterrupt:
+    print("Configuration dialogue has stopped.")
